94733ae87b0e435a18879c0e5d70792e304031df
OsamaGani <mohdusama0772>
2026-09-20 17:30:43 +0530

    changes in files

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -167,7 +167,7 @@ const Footer = () => {
       <div className="footer-bottom">
         <p>
           © Khel Aur Shiksha Foundation. All Rights Reserved – Develop by{" "}
-          <a href="#">Usama Gani kk</a>
+          <a href="#">Usama Gani</a>
         </p>
       </div>
     </footer>
