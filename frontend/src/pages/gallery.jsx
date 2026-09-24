969c516fb5e92378ed71aaec77ea18b746057222
OsamaGani <mohdusama0772@gmail.com>
2026-09-25 04:12:30 +0530

    Fix gallery production API URL

--- frontend/src/pages/gallery.jsx
+++ frontend/src/pages/gallery.jsx
@@ -7,7 +7,7 @@ import JoinCommunity from "../components/JoinCommunity";
 import "./gallery.css";
 
 const API_URL =
-  "import.meta.env.VITE_API_URL/api";
+  `${import.meta.env.VITE_API_URL}/api`;
 
 function Gallery() {
   const [galleryCards, setGalleryCards] = useState([]);
